aad126e09169a1e754d7589984a876c84fab260d
thisadityasoni <thisadityasoni@gmail.com>
2024-03-13 01:58:08 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask,request,render_template
 import re
+from flask import Flask, request, render_template, jsonify
 
+app = Flask(__name__)
+email_pattern = r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$'
 
-app=Flask(__name__)
+@app.route('/validate-email', methods=['POST'])
+def validate_email():
+    email = request.form.get('email')
+    if email:
+        if re.match(email_pattern, email):
+            return jsonify({'valid': True})
+    return jsonify({'valid': False})
 
-#Route and functionality
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index_fun():
-    if request.method=='POST':
-        r=0
+    if request.method == 'POST':
+        r = 0
         p = request.form["in_1"]
         s = request.form['in_2']
-        pattern=re.compile(p)
-        matches =[(ele.start(), ele.end()) for ele in re.finditer(pattern,s)]
-        r=int(len(matches))
+
+        if p == "email":
+            return render_template('index.html', string=s, regex=p)
+
+        pattern = re.compile(p)
+        matches = [(ele.start(), ele.end()) for ele in re.finditer(pattern, s)]
+        r = len(matches)
         return render_template('index.html', string=s, regex=p, match=matches, result=r)
     return render_template('index.html')
 
-#run the app
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
